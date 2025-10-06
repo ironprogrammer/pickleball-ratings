9538c8840738e08d6ecd36a745e634f20cf15e06
Brian Alexander <824344+ironprogrammer@users.noreply.github.com>
2025-10-06 12:56:29 -0700

    Set round-robin wrapper attribs automagically

--- src/round-robin/render.php
+++ src/round-robin/render.php
@@ -21,7 +21,7 @@ $players = max( 4, min( 32, $players ) );
 $courts  = max( 1, min( 8, $courts ) );
 ?>
 
-<div class="pbr-block pbr-block--round-robin">
+<div <?php echo get_block_wrapper_attributes( array( 'class' => 'pbr-block pbr-block--round-robin' ) ); ?>>
 	<div class="round-robin-container">
 		<div class="input-section">
 			<div class="input-group">
