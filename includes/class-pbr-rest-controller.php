dc5dfd9fde0f335d4b3c3ce70dda73fe6260e6f0
Brian Alexander <824344+ironprogrammer@users.noreply.github.com>
2025-10-08 14:57:18 -0700

    Rename methods to controller standard

--- includes/class-pbr-rest-controller.php
+++ includes/class-pbr-rest-controller.php
@@ -31,8 +31,8 @@ class PBR_REST_Controller {
 			'/player/(?P<dupr_id>[\w-]+)',
 			array(
 				'methods'             => WP_REST_Server::READABLE,
-				'callback'            => array( $this, 'get_player' ),
-				'permission_callback' => array( $this, 'get_player_permissions_check' ),
+				'callback'            => array( $this, 'get_item' ),
+				'permission_callback' => array( $this, 'get_item_permissions_check' ),
 				'args'                => array(
 					'dupr_id' => array(
 						'description'       => __( 'The DUPR ID of the player.', 'pickleball-ratings' ),
@@ -93,8 +93,8 @@ class PBR_REST_Controller {
 	 * @param WP_REST_Request $request Full details about the request.
 	 * @return WP_REST_Response|WP_Error Response object on success, or WP_Error object on failure.
 	 */
-	public function get_player( $request ) {
 		$dupr_id = $request->get_param( 'dupr_id' );
+	public function get_item( $request ) {
 		$api     = new PBR_DUPR_API();
 		$data    = $api->get_player_data( $dupr_id );
 
@@ -111,7 +111,7 @@ class PBR_REST_Controller {
 	 * @param WP_REST_Request $request Full details about the request.
 	 * @return true|WP_Error True if the request has read access, WP_Error object otherwise.
 	 */
-	public function get_player_permissions_check( $request ) { // phpcs:ignore Generic.CodeAnalysis.UnusedFunctionParameter.Found
+	public function get_item_permissions_check( $request ) { // phpcs:ignore Generic.CodeAnalysis.UnusedFunctionParameter.Found
 		if ( ! current_user_can( 'edit_posts' ) ) {
 			return new WP_Error( 'rest_forbidden', esc_html__( 'You do not have permissions to view player data.', 'pickleball-ratings' ), array( 'status' => 401 ) );
 		}
